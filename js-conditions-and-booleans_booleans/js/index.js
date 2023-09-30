38d9047464f680bcf95d2d788d4bd334bb581e7c
ragnamarie <ragna.steinboemer@gmail.com>
2023-09-30 10:25:29 +0200

    solved

--- js-conditions-and-booleans_booleans/js/index.js
+++ js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,37 @@
 console.clear();
 
-console.log(Boolean("Try it out"));
+//true
+console.log(Boolean(2 == 2));
+
+//false
+console.log(Boolean(2 === "2"));
+
+//true
+console.log(Boolean(2 === 2));
+
+//true
+console.log(Boolean("hello world"));
+
+//true (if space)
+console.log(Boolean(" "));
+
+//true
+console.log(Boolean(true));
+
+//false
+console.log(Boolean(0));
+
+//true
+console.log(Boolean(-50));
+
+//true
+console.log(Boolean(50));
+
+//true
+console.log(Boolean(5 > 1));
+
+//true
+console.log(Boolean("1 > 10"));
+
+//true
+console.log(Boolean("false"));
